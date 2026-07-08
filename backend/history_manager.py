5509e4cbaf4b0e07560cc1e5bebd8ba22e7d3f9d
aaazhuzong <1265011481@qq.com>
2026-07-08 15:39:25 +0800

    Update README.md

--- backend/history_manager.py
+++ backend/history_manager.py
@@ -5,10 +5,11 @@ import os
 JSON_PATH = "history_data.json"
 
 class Node:
-    def __init__(self, timestamp, img_path, text):
+    def __init__(self, timestamp, img_path, text, boxes=None):
         self.timestamp = timestamp
         self.img_path = img_path
         self.text = text
+        self.boxes = boxes if boxes is not None else []
         self.left = None
         self.right = None
 
@@ -17,8 +18,8 @@ class BSTHistory:
         self.root = None
         self.load_json()
 
-    def insert(self, timestamp, img_path, text):
-        new_node = Node(timestamp, img_path, text)
+    def insert(self, timestamp, img_path, text, boxes):
+        new_node = Node(timestamp, img_path, text, boxes)
         if self.root is None:
             self.root = new_node
         else:
@@ -48,7 +49,8 @@ class BSTHistory:
             res_list.append({
                 "time_stamp": node.timestamp,
                 "img_path": node.img_path,
-                "ocr_text": node.text
+                "ocr_text": node.text,
+                "text_boxes": node.boxes
             })
             self._inorder(node.right, res_list)
 
@@ -63,7 +65,8 @@ class BSTHistory:
                 res.append({
                     "time_stamp": node.timestamp,
                     "img_path": node.img_path,
-                    "ocr_text": node.text
+                    "ocr_text": node.text,
+                    "text_boxes": node.boxes
                 })
             self._search(node.left, key, res)
             self._search(node.right, key, res)
@@ -78,5 +81,25 @@ class BSTHistory:
             return
         with open(JSON_PATH, "r", encoding="utf-8") as f:
             data = json.load(f)
+        self.root = None
         for item in data:
-            self.insert(item["time_stamp"], item["img_path"], item["ocr_text"])
+            boxes = item.get("text_boxes", [])
+            node = Node(item["time_stamp"], item["img_path"], item["ocr_text"], boxes)
+            self._rebuild_insert(node)
+
+    def _rebuild_insert(self, new_node):
+        if self.root is None:
+            self.root = new_node
+            return
+        root = self.root
+        while True:
+            if new_node.timestamp < root.timestamp:
+                if root.left is None:
+                    root.left = new_node
+                    break
+                root = root.left
+            else:
+                if root.right is None:
+                    root.left = new_node
+                    break
+                root = root.right
